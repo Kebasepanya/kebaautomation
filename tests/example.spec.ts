c0e4371217da55e44d88da612c62086bce4bacb4
kebasepanya <Kebaabetswe1.sepanya@gmail.com>
2025-06-06 11:58:39 +0200

    Cart changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -20,4 +20,17 @@ await page.fill('input[name="password"]', password);
 //click the button to log in 
 await page.click('input[name="login-button"]');
   
+
+//ADDING ITEMS TO THE CART
+await page.goto('https://www.saucedemo.com/inventory.html');
+//Wait for the page to load fully 
+await page.waitForLoadState('networkidle')
+
+//Add two items to the cart
+await page.click('button[name="add-to-cart-sauce-labs-backpack"]');
+await page.click('button[name="add-to-cart-sauce-labs-bike-light"]');
+
+const cartBadge = page.locator('.shopping_cart_badge');
+await expect(cartBadge).toBeVisible();
+await expect(cartBadge).toHaveText('2');
 });
